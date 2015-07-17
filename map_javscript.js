f235ef0a6835d8dc7e5053acdcc683acca9ab345
Isaac Godfried <igodfried@isaac26.com>
2015-07-16 22:29:16 -0400

    fixes

--- map_javscript.js
+++ map_javscript.js
@@ -23,21 +23,25 @@ function initialize() {
   var veazieRight = '<p> <b> Veazie Right Channel </b> <div class="video-container"> <iframe width="390" height="219" src="https://www.youtube.com/embed/2A4nEaegNsU?rel=0" frameborder="0" allowfullscreen></iframe> </div> <img alt="Veazie Right Line" src="http://www.isaac26.com/uploads/veazieright.jpg" height="300" width="400"> </img>  </p>'
   var baseRight = '<p> <div style="width: 100%; display: table;"> <div style="display: table-row">  <div style="width: 600px; display: table-cell;"> <b> The far right channel </b> <img src="http://www.isaac26.com/uploads/basinright.jpg" height=225 width=300 /> <img><div> The far right channel is in general too shallow and rocky to paddle down (flows 2-6ft on the Eddington gage). However, in the event of major rainstorm it could be a sneak route alternative (since the rest of the river would likely be very high).  </div></div> <div style="display: table-cell;"> <b> The Right Channel </b> <div class="video-container"> <iframe width="300" height="174" src="https://www.youtube.com/embed/cDeSv5ykPFg?rel=0" frameborder="0" allowfullscreen></iframe> </div> The main right channel provides many interesting lines that one can paddle at a variety of flows. The furthest right portion of the channel features a large wave train and a few holes (flow dependent). This line is generally quick and doable  at lower flows (as featured in the video above which was shot at approximately 2.7  on the Eddington gage). However as the river rises a very large wave/hole develops that would likely swamp a boat if hit directly. Further to the left are two smaller drops. These drops require maneuvering and bracing off of rocks at low water and at high water provide a good alternative to the large waves on the right.  <a target="_blank" href="http://www.isaac26.com/uploads/rightd.jpg" > <img src="http://www.isaac26.com/uploads/rightd.jpg" height=225 width=300> </a>      <div>  </div> </div> </div> </div>  <p>';
   var baseLeft = '<p> <b> The Center left </b> <b> Far left rapids </b> <div class="video-container"> <iframe width="560" height="315" src="https://www.youtube.com/embed/wjOxc4mHZbk" frameborder="0" allowfullscreen></iframe> </div> <p> <p> Coming soon! </p> <p> Do you have videos/photos/descriptions of the left and far left lines of Basin Rapid? Please send them to igodfried@isaac26.com. You will be credited.  </p>';
-  var finishInfo = '<b> The finish/takeout </b> <p><img src="http://www.isaac26.com/uploads/salmon.jpg" height=225 width=300> </p> The finish and takeout for the downriver events is located (river left) at the Eddington Salmon Club in Eddington Maine.  <p>' 
-  var startInfo = '<p> <b> The Start </b> </p> <img src="http://www.isaac26.com/uploads/bin1.jpg" height=225 width=300> </img> <img src="http://www.isaac26.com/uploads/bin2.jpg" height=225 width=300> </p> <p> Binnette  Park in Old Town Maine serves as both the start line and race headquarters. Here the Penobscot is relatively calm but just shortly downstream racers will encounter the first rapids.  </p> <a href="https://www.google.com/maps/dir//Binette+Park,+Old+Town,+ME/@44.9377334,-68.7209467,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x4cafb0acfdc3f10f:0x4d81b1dbba0cd3ff!2m2!1d-68.6469577!2d44.9376358"> Get Directions </a> '
+  var finishInfo = '<b> The finish/takeout (all downriver races) </b> <p><img src="http://www.isaac26.com/uploads/salmon.jpg" height=225 width=300> </p> The finish and takeout for the downriver events is located (river left) at the Eddington Salmon Club in Eddington Maine.  <p>' 
+  var startInfo = '<p> <b> Start ( all DR races, except wildwater) </b> </p> <img src="http://www.isaac26.com/uploads/bin1.jpg" height=225 width=300> </img> <img src="http://www.isaac26.com/uploads/bin2.jpg" height=225 width=300> </p> <p> Binnette  Park in Old Town Maine serves as both the start line (for the canoe downriver races) and race headquarters. Here the Penobscot is relatively calm but just shortly downstream racers will encounter the first rapids.  </p> <a href="https://www.google.com/maps/dir//Binette+Park,+Old+Town,+ME/@44.9377334,-68.7209467,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x4cafb0acfdc3f10f:0x4d81b1dbba0cd3ff!2m2!1d-68.6469577!2d44.9376358"> Get Directions </a> '
   var updates = '<p> <a href="http://nationals-coverage.herokuapp.com"> More </a> </p>';
   var access = '<p> <b> Great Works access road </b> </p> <p> Use to get your boat after sprint or for practice runs at Great Works </p> <a href="https://www.google.com/maps/dir//44.9219737,-68.631868/@44.9220235,-68.6317355,291m/data=!3m1!1e3"> Get Directions </a> </p>';
   var photo1 = '<p> Good spot to watch the race and snap a photo. Also a nice spot to put in if you want to practice running the left lines at Great Works </p>'
   var veazieOverlook = '<p> A nice overlook to see the race from </p>'
   var veazieAccess = '<p> <b> Veazie Rapid Access road </b> </p> This is the closest road to the Veazie Rapid. It is possible to put in here to run the Veazie Rapid but it will require a bit of hiking/boat dragging. </p> <a href="https://www.google.com/maps/dir//44.8347157,-68.702853/@44.8337867,-68.70335,499m/data=!3m1!1e3"> Directions </a> </p>'
   var wildWaterStart = '<p> Start of the wildwater race at the Orono Power Plant. <a href = "https://www.google.com/maps/dir//44.8801166,-68.6643622/@44.88005,-68.6640901,107m/data=!3m1!1e3"> Directions</a> </p>'
-  var basinPark = 'Access to Basin Mill from dirt parking lot by the power plant. </p>'
+  var basinPark = 'Access to Basin Mill from dirt parking lot by the power plant. </p>' 
+  var startYouth = 'Start of the youth course at the Orono boat launch'
+  var finishY = 'Finish of the youth course at 1080 Main St Veazie Maine'
+  var watchL = 'Good spot to see the Veazie Rapids.'
+  var sprintStart = 'Start for the wildwater sprint race.'
   if(screen.width<420)
   { 
   startInfo = '<p> <b> The Start </b> </p> <img src="http://www.isaac26.com/uploads/bin1.jpg" height=108 width=145> </img> <img src="http://www.isaac26.com/uploads/bin2.jpg" height=108 width=145> </p> <p> Binnette  Park in Old Town Maine serves as both the start line and race headquarters. Here the Penobscot is relatively calm but just shortly downstream racers will encounter the first rapids.  </p>'
   baseRight = ' <div> <b> The far right channel </b> <img src="http://www.isaac26.com/uploads/basinright.jpg" height=108 width=145 /> <img><div> The far right channel is in general too shallow and rocky to paddle down (flows 2-6ft on the Eddington gage). However, in the event of major rainstorm it could be a sneak route alternative (since the rest of the river would likely be very high).  </div> <div> <b> The Right Channel </b> <iframe width="145" height="108" src="https://www.youtube.com/embed/cDeSv5ykPFg?rel=0" frameborder="0" allowfullscreen></iframe> The main right channel provides many interesting lines that one can paddle at a variety of flows. The furthest right portion of the channel features a large wave train and a few holes (flow dependent). This line is generally quick and doable  at lower flows (as featured in the video above which was shot at approximately 2.7  on the Eddington gage). However as the river rises a very large wave/hole develops that would likely swamp a boat if hit directly. Further to the left are two smaller drops. These drops require maneuvering and bracing off of rocks at low water and at high water provide a good alternative to the large waves on the right. <a target="_blank" href="http://www.isaac26.com/uploads/rightd.jpg" > <img src="http://www.isaac26.com/uploads/rightd.jpg" height=108 width=146> </a> </div> </div> '
-  gLeftInfo = ' <div> <b> The (main) Left Channel </b>    <a target="_blank" href="http://www.isaac26.com/uploads/leftside.jpg" > <img src="http://www.isaac26.com/uploads/leftside.jpg" height=108 width=145> </img>  </a>   </div> <div>  <b> The far left channel  </b> <div> <div class=video-container">  <iframe width="145" height="108" src="https://www.youtube.com/embed/y0MflUM7vyE?rel=0&t=03s" frameborder="0" allowfullscreen></iframe> </div></div> <iframe width="145" height="108" src="https://www.youtube.com/embed/rZ_-G3ycNmw?rel=0&t=03s" frameborder="0" allowfullscreen></iframe> <div> The far left channel can generally be run at medium to high flows with some rock dodging and bracing (5+ ft on the Eddington gage). However, lower then 5ft and i t becomes very difficult for larger boats to navigate down.     </div> '
-  finishInfo = '<b> The finish/takeout </b> <p><img src="http://www.isaac26.com/uploads/salmon.jpg" height=108 width=146> </p> The finish and takeout for the downriver events is located (river left) at the Eddington Salmon Club in Eddington Maine. In total the downriver  race course is approximately 9.4 miles. <p>' 
+  gLeftInfo = ' <div> <b> The (main) Left Channel </b>    <a target="_blank" href="http://www.isaac26.com/uploads/leftside.jpg" > <img src="http://www.isaac26.com/uploads/leftside.jpg" height=108 width=145> </img>  </a>   </div> <div>  <b> The far left channel  </b> <div> <div class=video-container">  <iframe width="145" height="108" src="https://www.youtube.com/embed/y0MflUM7vyE?rel=0&t=03s" frameborder="0" allowfullscreen></iframe> </div> </div> <iframe width="145" height="108" src="https://www.youtube.com/embed/rZ_-G3ycNmw?rel=0&t=03s" frameborder="0" allowfullscreen></iframe> <div> The far left channel can generally be run at medium to high flows with some rock dodging and bracing (5+ ft on the Eddington gage). However, lower then 5ft and i t becomes very difficult for larger boats to navigate down.     </div> '
+  finishInfo = '<b> The finish/takeout (all downriver races) </b> <p><img src="http://www.isaac26.com/uploads/salmon.jpg" height=108 width=146> </p> The finish and takeout for the downriver events is located (river left) at the Eddington Salmon Club in Eddington Maine. In total the downriver  race course is approximately 9.4 miles. <p>' 
   veazieRight = '<p> <b> Veazie Right Channel </b> <div class="video-container"> <iframe width="390" height="219" src="https://www.youtube.com/embed/2A4nEaegNsU?rel=0" frameborder="0" allowfullscreen></iframe> </div> <img alt="Veazie Right Line" src="http://www.isaac26.com/uploads/veazieright.jpg" height="109" width="145"> </img>  </p>'
    baseCent = '<p> <b> The Center Lines </b> </p>  <a target="_blank" href ="http://www.isaac26.com/uploads/cr.jpg">  <img src="http://www.isaac26.com/uploads/cr.jpg" height=108 width=145> </a> <div class="video-container"> <iframe width="390" height="219" src="https://www.youtube.com/embed/Wsr3nI0Crig?rel=0" frameborder="0" allowfullscreen></iframe> <div> High Water </div> </div id = "video-container" > <div class="video-container"><iframe width="640" height="360" src="https://www.youtube.com/embed/6bt5IPGeXps" frameborder="0" allowfullscreen></iframe> Very high water </div> <p>';
   }
@@ -62,7 +66,10 @@ function initialize() {
   infoContent.push(veazieAccess);
   infoContent.push(basinPark);
   infoContent.push(wildWaterStart);
-  
+  infoContent.push(sprintStart);
+  infoContent.push('HI');
+  infoContent.push(watchL);
+   
   
   // Icon Array Fill
   icons.push('http://maps.google.com/mapfiles/kml/paddle/grn-circle.png');
@@ -86,6 +93,9 @@ function initialize() {
   icons.push('http://isaac26.com/uploads/car2.png');
   icons.push('http://isaac26.com/uploads/car2.png');
   icons.push('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+  icons.push('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+  icons.push('http://maps.google.com/mapfiles/ms/icons/red-dot.png');
+   icons.push('http://isaac26.com/uploads/photo1.png');
  
   //
   var myLatlng= new google.maps.LatLng(44.938038, -68.645933);
@@ -110,6 +120,9 @@ function initialize() {
   arrayLocation[18] = new google.maps.LatLng(44.834929, -68.702493);
   arrayLocation[19] = new google.maps.LatLng(44.880743, -68.665470);
   arrayLocation[20] = new google.maps.LatLng(44.880082, -68.664014);
+  arrayLocation[21] = new google.maps.LatLng(44.922989, -68.634386);
+  arrayLocation[22] = new google.maps.LatLng(44.922989, -68.634386);
+  arrayLocation[23] = new google.maps.LatLng(44.832716, -68.702470);
   var newWindow = new google.maps.InfoWindow({
   content:basinInfo });
   var mapOptions = {
@@ -177,6 +190,8 @@ var zoom = map.getZoom();
           markerArray[18].setMap(map);
           markerArray[19].setMap(map);
            markerArray[20].setMap(map);
+            markerArray[21].setMap(map);
+             markerArray[23].setMap(map);
          
        }
        else
@@ -198,6 +213,7 @@ var zoom = map.getZoom();
         markerArray[18].setMap(null);
         markerArray[19].setMap(null);
         markerArray[20].setMap(null);
+        markerArray[23].setMap(null);
                
        }
 });
